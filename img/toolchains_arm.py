dc1b4983730476fbf0a023545a743b1dc9a28aef
wangnuannuan <1961295051@qq.com>
2018-07-13 15:27:00 +0800

    Update toolchains_arm.py

--- img/toolchains_arm.py
+++ img/toolchains_arm.py
@@ -1,3 +1,4 @@
+
 tempfile.mkstemp([suffix=”[, prefix=’tmp'[, dir=None[, text=False]]]])
 # mkstemp方法用于创建一个临时文件。该方法仅仅用于创建临时文件，调用tempfile.mkstemp函数后，返回包含两个元素的元组，
 # 第一个元素指示操作该临时文件的安全级别，第二个元素指示该临时文件的路径。参数suffix和prefix分别表示临时文件名称的后缀和前缀；
